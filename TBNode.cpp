62e9a7cc07f4721e0b1a64520a8961e20a9effc7
xiaoqi64 <xiaoqi64@outlook.com>
2017-04-02 15:47:59 +0800

    1

---
+++ TBNode.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+typedef char ElemType;
+typedef struct node {
+	ElemType data;
+	struct node *lchild;
+	struct node *rchild;
+}TBNode;
+void CreatBTNode() {
+
+}
+void FindNode() {
+
+}
+int BTNodeDepth() {
+
+}
+void DispBTNode() {
+
+}
